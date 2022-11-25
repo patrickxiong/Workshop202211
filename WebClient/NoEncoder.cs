4b9e6180da86434eb83e553aadd4ffe01dc7741d
Patrick Xiong <patrick.xiong@aristocrat.com>
2022-11-25 14:23:33 +1100

    No Encoder

---
+++ WebClient/NoEncoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    public class NoEncoder : IEncodeDecode
+    {
+        public string Decode(string input)
+        {
+            return input;
+        }
+
+        public string Encode(string input)
+        {
+            return input;
+        }
+    }
+}
